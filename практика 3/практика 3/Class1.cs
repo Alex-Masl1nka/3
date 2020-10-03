7176f724e1d27e13e39e2f82449a97f7a47e09ae
Alex-Masl1nka <65232033+Alex-Masl1nka@users.noreply.github.com>
2020-10-03 17:23:15 +0300

    3

---
+++ практика 3/практика 3/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+public abstract class Hill
+{
+	protected int x, y, r;
+	public Hill(int X, int Y, int R)
+	{
+		x = X; y = Y; r = R;
+	}
+	public int height
+	{ 
+		get ; set ; 
+	}
+    public int width
+	{
+		get ; set ;
+	}
+	abstract public void DrawFigure(Graphics graf);
+}
+public class Square:Hill
+{
+	SolidBrush br = new SolidBrush(Color.Gray);
+	public Square(int x, int y, int r) : base(x, y, r)
+	{ }
+    public override void DrawFigure(Graphics graf)
+    {
+		graf.FillRectangle(br, x, y, r * 2, r * 2);
+    }
+}
+public class Circle : Hill
+{
+	SolidBrush br = new SolidBrush(Color.Gray);
+	public Circle(int x, int y, int r) : base(x, y, r)
+	{ }
+	public override void DrawFigure(Graphics graf)
+	{
+		graf.FillEllipse(br, x, y, r * 2, r * 2);
+	}
+}
+public class Triangle : Hill
+{
+	SolidBrush br = new SolidBrush(Color.Gray);
+	public Triangle(int x, int y, int r) : base(x, y, r)
+	{ }
+	public override void DrawFigure(Graphics graf)
+	{
+		graf.FillEllipse(br, x, y, r * 2, r * 2);
+	}
+}
---
+++ практика 3/практика 3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace практика_3
+{
+    public partial class Form1 : Form
+    {
+        Hill[] body = new Hill[3];
+        public Form1()
+        {
+            InitializeComponent();
+            body[0] = new Circle(ClientSize.Width / 2, ClientSize.Height / 2, 25);
+            body[1] = new Square(ClientSize.Width / 2, ClientSize.Height / 2, 25);
+            body[2] = new Triangle(ClientSize.Width / 2, ClientSize.Height / 2, 25);
+            Invalidate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new string[] { "Окружность", "Квадрат","Треугольник" });
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+                body[0].DrawFigure(e.Graphics);
+            if (comboBox1.SelectedIndex == 1)
+                body[1].DrawFigure(e.Graphics);
+            if (comboBox1.SelectedIndex == 2)
+                body[2].DrawFigure(e.Graphics);
+        }
+
+    }
+}
